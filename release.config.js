d5a3629175f495967f47c9d5b995727a4900491c
Shaun Saker <sakershaun@gmail.com>
2024-05-20 13:08:52 +1000

    build: automated releases

---
+++ release.config.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+    '@eclass/semantic-release-netlify',
+  ],
+}
