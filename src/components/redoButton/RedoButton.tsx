4fbf2b28e3c0f09f281888dddd9934d60734aa59
Shaun Saker <sakershaun@gmail.com>
2024-05-21 10:49:08 +1000

    feat: add labels to undo and redo buttons

--- src/components/redoButton/RedoButton.tsx
+++ src/components/redoButton/RedoButton.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 import RedoIcon from '../../assets/icons/redo-icon.svg'
 import { Button } from '../button/Button'
+import { Typography } from '../typography/Typography'
 
 export const RedoButton = (): ReactElement => {
   const onClick = useCallback(() => {
@@ -12,10 +13,13 @@ export const RedoButton = (): ReactElement => {
   return (
     <StyledButton type="button" kind="secondary" onClick={onClick}>
       <RedoIcon />
+
+      <Typography kind="tiny">Redo</Typography>
     </StyledButton>
   )
 }
 
 const StyledButton = styled(Button)`
   width: auto;
+  gap: ${({ theme }) => theme.spacing.xs}px;
 `
--- src/components/undoButton/UndoButton.tsx
+++ src/components/undoButton/UndoButton.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 import UndoIcon from '../../assets/icons/undo-icon.svg'
 import { Button } from '../button/Button'
+import { Typography } from '../typography/Typography'
 
 export const UndoButton = (): ReactElement => {
   const onClick = useCallback(() => {
@@ -12,10 +13,13 @@ export const UndoButton = (): ReactElement => {
   return (
     <StyledButton type="button" kind="secondary" onClick={onClick}>
       <UndoIcon />
+
+      <Typography kind="tiny">Undo</Typography>
     </StyledButton>
   )
 }
 
 const StyledButton = styled(Button)`
   width: auto;
+  gap: ${({ theme }) => theme.spacing.xs}px;
 `
